27cff0fae20e5f305aad73f75f10362ea2520e10
Ageeva Elena <ageevaed90@mail.ru>
2022-12-14 15:10:38 +0300

    Импорт animations

--- Task5/src/app/app.module.ts
+++ Task5/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import {MatTabsModule} from '@angular/material/tabs';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { TabGroupAlignExampleComponent } from './tab-group-align-example/tab-group-align-example.component';
 
 @NgModule({
@@ -16,7 +17,8 @@ import { TabGroupAlignExampleComponent } from './tab-group-align-example/tab-gro
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatTabsModule
+    MatTabsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
